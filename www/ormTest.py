3a1ab02ebbc914d8678f59cadcbb78095d2053c7
Song Xiangming <544479929@qq.com>
2017-01-01 16:25:04 +0800

    add a not

--- www/ormTest.py
+++ www/ormTest.py
@@ -6,9 +6,10 @@ from models import User, Blog, Comment
 
 loop = asyncio.get_event_loop()
 async def test():
+	#schema.sql偷么建了个www-data用户我都没注意
 	await orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
 	
-	u = User(name='Test', email='test@example.com', passwd='123456', image='about:blank')
+	u = User(name='Test2', email='test@example.com', passwd='123456', image='about:blank')
 	
 	await u.save()
 
