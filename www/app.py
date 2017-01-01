17233e730868d1226ffd2c611f28345b8728bd6c
Song Xiangming <544479929@qq.com>
2017-01-01 16:21:03 +0800

    add coroweb.py, update app.py:add middleware and init_jinja2

--- www/app.py
+++ www/app.py
@@ -13,15 +13,137 @@ import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
 
-def index(request):
-	#如果要在网络上传输，或者保存到磁盘上，就需要把str变为以字节为单位的bytes
-	#如果不加上content_type，进入网页会自动下载文件（一行字符：<h1>Awesome</h1>）
-	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html', charset='UTF-8')
+import orm
+from coroweb import add_routes, add_static
+
+'''
+一个middleware可以改变URL的输入、输出，甚至可以决定不继续处理而直接返回。
+middleware的用处就在于把通用的功能从每个URL处理函数中拿出来，集中放到一个地方。
+例如，一个记录URL日志的logger可以简单定义如下：
+'''
+async def logger_factory(app,handler):
+	async def logger(request):
+		logging.info('Request: %s %s' % (request.method, request.path))
+		return (await handler(request))
+	return logger
 	
+'''
+可参考灰_手的回答：你可以参考我的data_factory的实现。
+如果method == 'GET'时，参数就是查询字符串，也就是request.query_string
+如果method == 'POST'时，有两种可能，Ajax的json和html的form(表单)，分别对应request.json()和request.post()。 
+data_factory的主要作用就是把这些参数统一绑定在request.__data__上。
+'''
+#day5暂时未使用
+async def data_factory(app, handler):
+	async def parse_data(request):
+		if request.method == 'POST':
+			if request.content_type.startswith('application/json'):
+				request.__data__ = await request.json()
+				logging.info('request json: %s' % str(request.__data__))
+			elif request.content_type.startswith('application/x-www-form-urlencoded'):
+				request.__data__ = await request.post()
+				logging.info('request form: %s' % str(request.__data__))
+
+#response这个middleware把返回值转换为web.Response对象再返回，以保证满足aiohttp的要求：
+async def response_factory(app, handler):
+	async def response(request):
+		logging.info('Response handler...')
+		r = await handler(request)
+		#A StreamReader instance, input stream for reading request’s BODY.官方文档中的标准返回值web.Response()的类型
+		if isinstance(r, web.StreamResponse):
+			return r
+		#任意的二进制数据 application/octet-stream，要加上content_type
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r, str):
+			#重定向请求，302已找到
+			if r.startswith('redirect:'):
+				return web.HTTPFound(r[9:])
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		#没懂
+		if isinstance(r, dict):
+			template = r.get('__template__')
+			if template is None:
+				resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+				resp.content_type = 'application/json;charset=utf-8'
+				return resp
+			else:
+				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
+		#Http Response Code状态码范围100~600
+		if isinstance(r, int) and r >= 100 and r < 600:
+			return web.Response(r)
+		#目测是状态码 + description
+		if isinstance(r, tuple) and len(r) == 2:
+			t, m = r
+			if isinstance(t, int) and t >= 100 and t < 600:
+				return web.Response(t, str(m))
+		# default:
+		resp = web.Response(body=str(r).encode('utf-8'))
+		resp.content_type = 'text/plain;charset=utf-8'
+		return resp
+	return response
+
+def datetime_filter(t):
+		delta = int(time.time() - t)
+		if delta < 60:
+			return u'1分钟前'
+		if delta < 3600:
+			return u'%s分钟前' % (delta // 60)
+		if delta < 86400:
+			return u'%s小时前' % (delta // 3600)
+		if delta < 604800:
+			return u'%s天前' % (delta // 86400)
+		dt = datetime.fromtimestamp(t)
+		return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+# 初始化jinja2模板
+def init_jinja2(app, **kw):
+	logging.info('init jinja2...')
+	options = dict(
+		autoescape = kw.get('autoescape', True),
+		block_start_string = kw.get('block_start_string', '{%'),
+		block_end_string = kw.get('block_end_string', '%}'),
+		variable_start_string = kw.get('variable_start_string', '{{'),
+		variable_end_string = kw.get('variable_end_string', '}}'),
+		auto_reload = kw.get('auto_reload', True)
+	)
+	path = kw.get('path', None)
+	if path is None:
+		path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+	logging.info('set jinja2 template path: %s' % path)
+	# 在这里初始化模版配置
+	env = Environment(loader=FileSystemLoader(path), **options)
+	filters = kw.get('filters', None)
+	if filters is not None:
+		for name, f in filters.items():
+			env.filters[name] = f
+	app['__templating__'] = env
+		
+'''
+def index(request):
+	# 如果要在网络上传输，或者保存到磁盘上，就需要把str变为以字节为单位的bytes
+	# 如果不加上content_type，进入网页会自动下载文件（一行字符：<h1>Awesome</h1>）
+	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html', charset='UTF-8')
+'''
 async def init(loop):
-	app = web.Application(loop=loop)
-	app.router.add_route('GET','/',index)
+	await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='123456', db='awesome')
+	# Day5 在app.py中加入middleware、jinja2模板和自注册的支持
+	# logger_factory, response_factory是两个拦截器，init_jinja2初始化jinja2，这3个都在上方实现
+	app = web.Application(loop=loop, middlewares=[
+		logger_factory, response_factory
+	])
+	init_jinja2(app, filters=dict(datetime=datetime_filter))
+	add_routes(app, 'handlers')
+	add_static(app)
+	
 	srv = await loop.create_server(app.make_handler(),'127.0.0.1',8000)
 	logging.info('Server started at http://127.0.0.1:8000...')
 	return srv
